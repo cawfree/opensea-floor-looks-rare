928e25082be564413b222b9ecc47b9999b383f27
cawfree <alex@edgeandnode.com>
2021-12-16 20:26:37 +0000

    chore: base

---
+++ src/index.ts
@@ -0,0 +1,103 @@
+import axios from 'axios';
+import chalk from 'chalk';
+import puppeteer from 'puppeteer';
+import {scrollPageToBottom} from 'puppeteer-autoscroll-down';
+import {printTable} from 'console-table-printer';
+
+
+// Assume one-to-one with OpenSea?
+const collection_slug = 'boredapeyachtclub';
+const stepsLimit = 15;
+
+const collect = ({page, x}: {
+  readonly page: puppeteer.Page;
+  readonly x: string;
+}) => {
+  return page.$x(x)
+    .then(els => Promise.all(
+      els.map(el => page.evaluate(el => el.textContent, el)),
+    ));
+};
+
+const colorForRank = (rank: string) => {
+  const ranking = parseInt(rank.substring(1).trim());
+  if (ranking < 2000) {
+    return chalk.green;
+  } else if (ranking < 6000) {
+    return chalk.yellow;
+  }
+  return chalk.white;
+};
+
+void (async () => {
+
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+
+  await page.goto(
+    `https://opensea.io/collection/${collection_slug}?search[sortAscending]=true&search[sortBy]=PRICE&search[toggles][0]=BUY_NOW`
+  );
+
+  const namesToPrices = {} as Record<string, string>;
+
+  for (let i = 0; i < stepsLimit; i += 1) {
+    await scrollPageToBottom(page, {
+      size: 500,
+      delay: 240,
+      stepsLimit: 1,
+    });
+    const [
+      // HACK: Assume names map to identifiers. This isn't always true!
+      names,
+      prices,
+    ] = await Promise.all([
+      collect({page, x: "//div[contains(@class, 'AssetCardFooter--name')]"}),
+      collect({page, x: "//div[contains(@class, 'AssetCardFooter--price')]"})
+        .then(arr => arr.filter((_, i) => i % 2 === 1)),
+    ]);
+    for (let j = 0; j < names.length; j += 1) {
+      const name = names[j];
+      namesToPrices[name] = prices[j];
+    }
+  }
+
+  // Next, determine their rarity.
+  const results = await Promise.all(
+    Object.entries(namesToPrices)
+      .map(async ([k, v]) => {
+        const page = await browser.newPage();
+        await page.goto(`https://rarity.tools/${collection_slug}/view/${k}`);
+        const x = "//span[contains(@class, 'font-bold whitespace-nowrap')]";
+        await page.waitForXPath(x);
+        const [res] = await collect({page, x});
+        if (res) {
+          const rank = res.substring('Rarity Rank: '.length);
+          return [k, v, rank];
+        }
+        return null;
+      }),
+  );
+
+  await browser.close();
+
+  console.clear();
+  console.log();
+  console.log();
+  console.log(`🛳️  OpenSea floor rarity for ${chalk.bold(collection_slug)}:`);
+  console.log();
+  console.log();
+
+  printTable(
+    results.map(([id, price, rank]) => ({
+      'Token ID': `#${id}`,
+      'Price': `${price.trim()}Ξ`,
+      'Ranking on Rarity Tools': colorForRank(rank)(rank),
+    })),
+  );
+
+  console.log();
+  console.log();
+
+})();
+
+
